645e0852e5be82a0af1e7ac3eb210f006621c9e0
Vikranth Akarapu <rohitakarapu>
2017-10-28 12:11:29 -0500

    changed

---
+++ javascript/first.js
@@ -0,0 +1,18 @@
+var a=17
+if(a>=18){
+    alert ("Candidate is eligible to vote")
+}else{
+    alert ("candidate is not eligible to vote")
+}
+
+
+
+
+var a=1
+if (a>0){
+    console.log ("It is a positive number")
+} else if (a<0){
+    console.log("It is a negative number")
+}else{
+    console.log("It is Zero")
+}
